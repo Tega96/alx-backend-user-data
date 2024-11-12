5ac8ef6b560741f34194eb6e6b3ee1c3a7378f14
Tega96 <otiteotega@gmail.com>
2024-11-12 22:19:24 +0000

    tasks 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -25,18 +25,51 @@ elif AUTH_TYPE == 'basic_auth':
     auth = BasicAuth()
 
 
+@app.before_request
+def before_request():
+    """
+        _summary_
+
+    Returns:
+        _type_: _description_
+    """
+    if auth is None:
+        pass
+    else:
+        excluded_list == ['/api/v1/status/',
+                            '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+        if auth.require_auth(request.path, excluded_list):
+            if auth.authorization_header(request) is None:
+                abort(401, description ="Unautorized")
+            if auth.current_user(request) is None:
+                abort(403, description='Forbidden')
+
+
+@app.errorhandler(401)
+def unauthorised(error) -> str:
+    """ Unauthorised handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+def forbidden(error) -> str:
+    """_summary_
+
+    Args:
+        error (_type): _description_
+
+    Returns:
+        str: _description_
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
-
-
-@app.errorhandler(401)
-def unauthorised(erro) -> str:
-    """ Unauthorised handler
-    """
-    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
